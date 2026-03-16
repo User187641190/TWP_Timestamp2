7f16efc0a6fffba63eb4025c0328e13788383c06
KKU_NattapongCham <nattapong.cham@kkumail.com>
2026-03-17 01:31:39 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,25 +1,19 @@
-
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-MYSQLHOST = os.getenv("MYSQLHOST")
-MYSQLUSER = os.getenv("MYSQLUSER")
-MYSQLPASSWORD = os.getenv("MYSQLPASSWORD")
-MYSQLDATABASE = os.getenv("MYSQLDATABASE")
-MYSQLPORT = os.getenv("MYSQLPORT")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:GcWFEBxxoWfBGptDERoJDJVAeFauNyDd@switchyard.proxy.rlwy.net:28218/railway"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
-
 
 Base = declarative_base()
 
